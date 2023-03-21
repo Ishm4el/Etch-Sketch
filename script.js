0a3c5727ca8b0433cde22098e0e97fcb55fc7671
Ish <ismaelv@protonmail.com>
2023-03-21 02:07:27 -0700

    Implemented the resize/reset button.

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('style', "display: flex; flex: 1 1 1px; flex-direction: column;");
-    for (let j = 0; j < 16; j++) {
-        const pix = document.createElement('div');
-        pix.setAttribute('style', "background-color: white; flex: 1 1 1px;");
-        pix.addEventListener(
-            "mouseover",
-            (event) => {
-                event.target.style.backgroundColor = "black";
-            },
-            false
-        );
+const maxSize = 100;
+let size = 16;
 
-        row.appendChild(pix);
+const refactor = document.querySelector('#refactor');
+refactor.addEventListener('click', () => {
+    size = prompt("Please enter a new size for the canvas: (max is 100!)");
+    removeChildren(container);
+    draw(size, maxSize);
+});
+
+function draw(size, maxSize) {
+    if (maxSize >= size) {
+        for (let i = 0; i < size; i++) {
+            const row = document.createElement('div');
+            row.setAttribute('style', "display: flex; flex: 1 1 1px; flex-direction: column;");
+            for (let j = 0; j < size; j++) {
+                const pix = document.createElement('div');
+                pix.setAttribute('style', "background-color: white; flex: 1 1 1px;");
+                pix.addEventListener(
+                    "mouseover",
+                    (event) => {
+                        event.target.style.backgroundColor = "black";
+                    },
+                    false
+                );
+    
+                row.appendChild(pix);
+            }
+            container.appendChild(row);
+        }
     }
-    container.appendChild(row);
 }
+
+function removeChildren(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+draw(size, maxSize);
