5fe0c2933a797260d6596711e1e13af362e6f50a
Ish <ismaelv@protonmail.com>
2023-03-21 01:43:39 -0700

    Forumalted the method upon which the grid will be displayed. Next, to add hover effect

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.setAttribute('style', "display: flex; flex: 1 1 1px; flex-direction: column;");
+    for (let j = 0; j < 16; j++) {
+        const pix = document.createElement('div');
+        pix.setAttribute('style', "background-color: white; flex: 1 1 1px;");
+
+        row.appendChild(pix);
+    }
+    container.appendChild(row);
+}
